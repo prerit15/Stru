7c588470b4f3888a885996610594af9fc6c1ab92
Prerit <prerit15@gmail.com>
2013-04-14 13:03:46 -0800

    Welcome to egit eclipse

---
+++ MyHellowworld/src/com/git/Hello.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+System.out.println("hello prerit welcome to e git");
+	}
+
+}
---
+++ MyHellowworld/src/com/git/Namaste.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Namaste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	System.out.println("namaste");
+
+	}
+
+}
